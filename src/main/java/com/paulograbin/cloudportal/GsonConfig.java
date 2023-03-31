a4ce07b2033c80da9edc55bf43c6e70a0f62e9e4
Paulo Grolli Grabin <paulograbin@gmail.com>
2023-03-31 19:54:57 -0300

    Include GSON to parse the files and return them as objects

---
+++ src/main/java/com/paulograbin/cloudportal/GsonConfig.java
@@ -0,0 +1,23 @@
+package com.paulograbin.cloudportal;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
+@Configuration
+public class GsonConfig {
+
+    @Bean
+    public Gson gson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter());
+        gsonBuilder.registerTypeAdapter(OffsetDateTime.class, new LocalDateTimeTypeAdapter());
+
+        return gsonBuilder.create();
+    }
+}
---
+++ src/main/java/com/paulograbin/cloudportal/LocalDateTimeTypeAdapter.java
@@ -0,0 +1,36 @@
+package com.paulograbin.cloudportal;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
+
+public class LocalDateTimeTypeAdapter extends TypeAdapter<OffsetDateTime> {
+    private final Logger LOG = LoggerFactory.getLogger(LocalDateTimeTypeAdapter.class);
+
+
+    @Override
+    public void write(final JsonWriter jsonWriter, final OffsetDateTime localDate) throws IOException {
+        if (localDate == null) {
+            jsonWriter.nullValue();
+            return;
+        }
+        jsonWriter.value(localDate.toString());
+    }
+
+    @Override
+    public OffsetDateTime read(final JsonReader jsonReader) throws IOException {
+        if (jsonReader.peek() == JsonToken.NULL) {
+            jsonReader.nextNull();
+            return null;
+        }
+        String text = jsonReader.nextString();
+        return ZonedDateTime.parse(text).toOffsetDateTime();
+    }
+}
