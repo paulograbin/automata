b45d6cae4bacd056ec3f46b50a966600fc28f1c1
Paulo Grolli Grabin <paulograbin@gmail.com>
2023-04-02 18:41:32 -0300

    Format code

--- src/main/java/com/paulograbin/cloudportal/HomeController.java
+++ src/main/java/com/paulograbin/cloudportal/HomeController.java
@@ -1,8 +1,8 @@
 package com.paulograbin.cloudportal;
 
- import com.paulograbin.ccv2api.model.BuildDetailsDTO;
- import com.paulograbin.ccv2api.model.DeploymentDetailsDTO;
- import org.springframework.stereotype.Controller;
+import com.paulograbin.ccv2api.model.BuildDetailsDTO;
+import com.paulograbin.ccv2api.model.DeploymentDetailsDTO;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,32 +10,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class HomeController
-{
+public class HomeController {
 
-	private final BuildService buildService;
-	private final DeploymentService deploymentService;
-	private final EnvironmentService environmentService;
+    private final BuildService buildService;
+    private final DeploymentService deploymentService;
+    private final EnvironmentService environmentService;
 
-	public HomeController(BuildService buildService, DeploymentService deploymentService, EnvironmentService environmentService)
-	{
-		this.buildService = buildService;
-		this.deploymentService = deploymentService;
-		this.environmentService = environmentService;
-	}
+    public HomeController(BuildService buildService, DeploymentService deploymentService, EnvironmentService environmentService) {
+        this.buildService = buildService;
+        this.deploymentService = deploymentService;
+        this.environmentService = environmentService;
+    }
 
-	@GetMapping
-	public String home(Model model)
-	{
-		model.addAttribute("environments", environmentService.fetchAllEnvironments());
+    @GetMapping
+    public String home(Model model) {
+        model.addAttribute("environments", environmentService.fetchAllEnvironments());
 
-		BuildDetailsDTO allBuilds = buildService.getAllBuilds();
-		model.addAttribute("builds", allBuilds);
+        BuildDetailsDTO allBuilds = buildService.getAllBuilds();
+        model.addAttribute("builds", allBuilds);
 
-		DeploymentDetailsDTO deploymentDetailsDTO = deploymentService.fetchDeployments();
-		model.addAttribute("deployments", deploymentDetailsDTO);
+        DeploymentDetailsDTO deploymentDetailsDTO = deploymentService.fetchDeployments();
+        model.addAttribute("deployments", deploymentDetailsDTO);
 
-		return "index.html";
-	}
+        return "index.html";
+    }
 
 }
