809e65b7a6187369390eb870abe31b56dd7d638f
Paulo Grolli Grabin <paulograbin@gmail.com>
2023-04-02 19:47:47 -0300

    Display log with alert time

--- src/main/java/com/paulograbin/cloudportal/AlertService.java
+++ src/main/java/com/paulograbin/cloudportal/AlertService.java
@@ -1,14 +1,23 @@
 package com.paulograbin.cloudportal;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 @Service
 public class AlertService {
 
+    private final Logger LOG = LoggerFactory.getLogger(BuildService.class);
+
 
     public void sendAlert(String message) {
+        LOG.info("Sending alert: {} at {}", message,  new SimpleDateFormat("HH:mm:ss").format(new Date()));
+
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.postForObject("http://ntfy.sh/builds", message, String.class);
     }
