29b574e111b33b9bbff37e9dcd99f48c0e73f23f
Paulo Grolli Grabin <paulograbin@gmail.com>
2023-04-02 12:32:34 -0300

    Rename variables and change messages

--- src/main/java/com/paulograbin/cloudportal/DeploymentService.java
+++ src/main/java/com/paulograbin/cloudportal/DeploymentService.java
@@ -16,14 +16,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class DeploymentService {
 
-	private final Logger LOG = LoggerFactory.getLogger(DeploymentService.class);
+    private final Logger LOG = LoggerFactory.getLogger(DeploymentService.class);
 
-	private final CloudPortalOperations cloudPortalOperations;
+    private final CloudPortalOperations cloudPortalOperations;
 
 
-	public DeploymentService(CloudPortalOperations cloudPortalOperations) {
-		this.cloudPortalOperations = cloudPortalOperations;
-	}
+    public DeploymentService(CloudPortalOperations cloudPortalOperations) {
+        this.cloudPortalOperations = cloudPortalOperations;
+    }
 
     public void makeDeployment(String buildCode) {
         CreateDeploymentRequestDTO request = new CreateDeploymentRequestDTO();
@@ -32,29 +32,27 @@ public class DeploymentService {
         request.setDatabaseUpdateMode(CreateDeploymentRequestDTO.DatabaseUpdateModeEnum.NONE);
         request.setEnvironmentCode("d1");
 
-		CreateDeploymentResponseDTO deployments = cloudPortalOperations.sendPostRequest("deployments", request, CreateDeploymentResponseDTO.class);
+        CreateDeploymentResponseDTO deployments = cloudPortalOperations.sendPostRequest("deployments", request, CreateDeploymentResponseDTO.class);
 
-		LOG.info("Deployment created: ");
-		LOG.info(" Code: {}", deployments.getCode());
-	}
+        LOG.info("Deployment created: ");
+        LOG.info(" Code: {}", deployments.getCode());
+    }
 
     @Cacheable("deployments")
     public DeploymentDetailsDTO fetchDeployments() {
         LOG.info("Fetching all recent deployments...");
 
-			DeploymentDetailsDTO buildDetailsDTO = cloudPortalOperations.getDeployments("deployments");
+        DeploymentDetailsDTO deploymentDetails = cloudPortalOperations.getDeployments("deployments");
 
-			for (DeploymentDetailDTO buildDetailDTO : buildDetailsDTO.getValue())
-			{
-				LOG.info(" ****************** ");
-				LOG.info("Deployment details: ");
-				LOG.info(" Code {}", buildDetailDTO.getCode());
-				LOG.info(" Code {}", buildDetailDTO.getEnvironmentCode());
-				LOG.info(" Status {}", buildDetailDTO.getStatus());
-				LOG.info(" Created by {}", buildDetailDTO.getCreatedBy());
-			}
+        for (DeploymentDetailDTO buildDetailDTO : deploymentDetails.getValue()) {
+            LOG.info(" ****************** ");
+            LOG.info("Deployment details: ");
+            LOG.info(" Code {}", buildDetailDTO.getCode());
+            LOG.info(" Code {}", buildDetailDTO.getEnvironmentCode());
+            LOG.info(" Status {}", buildDetailDTO.getStatus());
+            LOG.info(" Created by {}", buildDetailDTO.getCreatedBy());
+        }
 
-			return buildDetailsDTO;
-		}
-	}
+        return deploymentDetails;
+    }
 }
