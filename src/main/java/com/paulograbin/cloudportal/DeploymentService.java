1a7fa5eb402ca11f7747087bf1856b0b09306d5b
Paulo Grolli Grabin <paulograbin@gmail.com>
2023-04-02 19:50:32 -0300

    Method to monitor deployments and sending alerts

--- src/main/java/com/paulograbin/cloudportal/DeploymentService.java
+++ src/main/java/com/paulograbin/cloudportal/DeploymentService.java
@@ -4,6 +4,7 @@ import com.paulograbin.ccv2api.model.CreateDeploymentRequestDTO;
 import com.paulograbin.ccv2api.model.CreateDeploymentResponseDTO;
 import com.paulograbin.ccv2api.model.DeploymentDetailDTO;
 import com.paulograbin.ccv2api.model.DeploymentDetailsDTO;
+import com.paulograbin.ccv2api.model.DeploymentProgressDTO;
 import com.paulograbin.cloudportal.ccv2.CloudPortalOperations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,10 +21,12 @@ public class DeploymentService {
     private final Logger LOG = LoggerFactory.getLogger(DeploymentService.class);
 
     private final CloudPortalOperations cloudPortalOperations;
+    private final AlertService alertService;
 
 
-    public DeploymentService(CloudPortalOperations cloudPortalOperations) {
+    public DeploymentService(CloudPortalOperations cloudPortalOperations, AlertService alertService) {
         this.cloudPortalOperations = cloudPortalOperations;
+        this.alertService = alertService;
     }
 
     public CreateDeploymentResponseDTO makeDeployment(String buildCode, String environmentCode) {
@@ -80,5 +83,30 @@ public class DeploymentService {
         }
 
         return deploymentDetails;
+    }
+
+    public void monitorDeployment(String code) throws InterruptedException {
+        DeploymentProgressDTO deploymentProgress = cloudPortalOperations.getDeploymentProgress(code);
+
+        String message = "";
+
+        LOG.info("Build {} has status {}", deploymentProgress.getDeploymentCode(), deploymentProgress.getDeploymentStatus());
+
+        if (deploymentProgress.getDeploymentStatus().equalsIgnoreCase("DEPLOYED")) {
+            message = deploymentProgress.getDeploymentCode() + " ready";
+        } else if (deploymentProgress.getDeploymentStatus().equalsIgnoreCase("FAIL")) {
+            message = deploymentProgress.getDeploymentCode() + " failed";
+        } else if (deploymentProgress.getDeploymentStatus().equalsIgnoreCase("DEPLOYING")) {
+            message = deploymentProgress.getDeploymentCode() + " is building, at " + deploymentProgress.getPercentage() + "%";
+        } else if (deploymentProgress.getDeploymentStatus().equalsIgnoreCase("SCHEDULED")) {
+            message = deploymentProgress.getDeploymentCode() + " building not started yet";
+        } else {
+            message = deploymentProgress.getDeploymentCode() + " new status " + deploymentProgress.getDeploymentStatus();
+        }
+
+        alertService.sendAlert(message);
+        Thread.sleep(5 * 60 * 1000);
+
+        monitorDeployment(code);
     }
 }
