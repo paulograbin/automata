b4c6892967015ee417fc6e08228c25c4b1e603f9
Paulo Grabin <paulograbin@gmail.com>
2023-04-04 10:45:00 -0300

    Split main class into two, adding new event listener

--- src/main/java/com/paulograbin/cloudportal/App.java
+++ src/main/java/com/paulograbin/cloudportal/App.java
@@ -11,99 +11,23 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class App implements CommandLineRunner {
+public class App {
 
-	private static final Logger LOG = LoggerFactory.getLogger(BuildService.class);
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
-    private final BuildService buildService;
-    private final DeploymentService deploymentService;
-    private final EnvironmentService environmentService;
-    private final AlertService alertService;
-
-
-    public App(BuildService buildService, DeploymentService deploymentService, EnvironmentService environmentService, AlertService alertService) {
-        this.buildService = buildService;
-        this.deploymentService = deploymentService;
-        this.environmentService = environmentService;
-        this.alertService = alertService;
-    }
 
 	public static void main(String[] args) {
-		for (String arg : args) {
-			LOG.info("Arg {}", arg);
-		}
-
 		if (args.length > 0) {
-			LOG.info("Found args, so do not start web server");
+			LOG.warn("Starting app in commandline mode");
 
 			new SpringApplicationBuilder(App.class)
 					.web(WebApplicationType.NONE)
+					.profiles("commandline")
+					.headless(true)
 					.run(args);
-
 		} else {
-			LOG.info("Found no args, so start web server...");
+			LOG.info("Starting app normally...");
 			SpringApplication.run(App.class, args);
 		}
 	}
-
-    @Override
-    public void run(String... args) throws Exception {
-        Instant tenStart = Instant.now();
-        BuildDetailsDTO last10Builds = buildService.getLast10Builds();
-        long tenDuration = Duration.between(tenStart, Instant.now()).toMillis();
-
-        Instant allStart = Instant.now();
-        buildService.fetchAllBuilds();
-        long allDuration = Duration.between(allStart, Instant.now()).toMillis();
-
-        LOG.info("10 builds took {} ms", tenDuration);
-        LOG.info("All builds took {} ms", allDuration);
-
-
-        for (var recentBuild : last10Builds.getValue())
-        {
-            if (recentBuild.getStatus().equalsIgnoreCase("BUILDING") )
-            {
-				new Thread(() ->
-				{
-					LOG.info("WILL MONITOR BUILD {}", recentBuild.getCode());
-					buildService.monitorBuild(recentBuild.getCode());
-				}).start();
-            }
-        }
-
-		DeploymentDetailsDTO lastDeployments = deploymentService.fetchDeployments();
-		for (var deployment : lastDeployments.getValue())
-		{
-			if (deployment.getStatus().equalsIgnoreCase("DEPLOYING"))
-			{
-				new Thread(() ->
-				{
-					LOG.info("WILL MONITOR DEPLOYMENT {}", deployment.getCode());
-					deploymentService.monitorDeployment(deployment.getCode());
-				}).start();
-			}
-		}
-
-		EnvironmentsDTO environments = environmentService.fetchAllEnvironments();
-
-        for (EnvironmentDTO environment : environments.getValue()) {
-            DeploymentDetailsDTO deployed = deploymentService.fetchDeploymentPerEnvironment(environment.getCode());
-
-            List<DeploymentDetailDTO> deployedDetails = deployed.getValue();
-
-            if (deployedDetails.size() == 1) {
-                BuildDetailDTO buildDetails = buildService.getBuildDetails(deployedDetails.get(0).getBuildCode());
-
-                LOG.info("Environment {} has build {}-{}", environment.getCode(), buildDetails.getCode(), buildDetails.getName());
-            }
-        }
-
-        LOG.info(" ******************* ");
-        LOG.info(" APP READY ");
-        LOG.info(" APP READY ");
-        LOG.info(" APP READY ");
-        LOG.info(" APP READY ");
-        LOG.info(" ******************* ");
-    }
 }
