f96f8906d09a1275c419a8c0da359e0c3a8a46d2
Paulo Grolli Grabin <paulograbin@gmail.com>
2023-04-07 15:21:39 -0300

    HomeController now uses async code to fill model with attributes

--- src/main/java/com/paulograbin/cloudportal/web/HomeController.java
+++ src/main/java/com/paulograbin/cloudportal/web/HomeController.java
@@ -3,15 +3,25 @@ package com.paulograbin.cloudportal.web;
 import com.paulograbin.cloudportal.BuildService;
 import com.paulograbin.cloudportal.DeploymentService;
 import com.paulograbin.cloudportal.EnvironmentService;
+import com.paulograbin.cloudportal.ccv2.dto.BuildDetailsDTO;
+import com.paulograbin.cloudportal.ccv2.dto.DeploymentDetailsDTO;
+import com.paulograbin.cloudportal.ccv2.v1dto.EnvironmentsDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
 
     private final BuildService buildService;
     private final DeploymentService deploymentService;
@@ -25,13 +35,33 @@ public class HomeController {
 
     @GetMapping
     public String home(Model model) {
-        model.addAttribute("environments", environmentService.fetchAllEnvironments());
+        CompletableFuture<EnvironmentsDTO> environmentsFuture = environmentService.fetchAllEnvironments();
+        CompletableFuture<BuildDetailsDTO> last10Builds = buildService.getLast10Builds();
+        CompletableFuture<DeploymentDetailsDTO> deployments = deploymentService.fetchCurrentDeployments();
+
+        environmentsFuture.thenAccept(env -> {
+            model.addAttribute("environments", env);
+        });
+
+        last10Builds.thenAccept(builds -> {
+            model.addAttribute("builds", builds);
+        });
+
+        deployments.thenAccept(deploys -> {
+            model.addAttribute("deployments", deploys);
+        });
+
+        Stream.of(environmentsFuture, environmentsFuture, last10Builds, deployments)
+                .map(CompletableFuture::join)
+                .forEach(l -> LOG.info("Something completed: {}", l));
+
+//        model.addAttribute("environments", environmentService.fetchAllEnvironmentsSync());
 //        model.addAttribute("environments", new EnvironmentsDTO());
 
-        model.addAttribute("builds", buildService.getLast10Builds());
+//        model.addAttribute("builds", buildService.getLast10BuildsNotAsync());
 //        model.addAttribute("builds", new com.paulograbin.cloudportal.ccv2.dto.BuildDetailsDTO());
 
-        model.addAttribute("deployments", deploymentService.fetchCurrentDeployments());
+//        model.addAttribute("deployments", attributeValue);
 //        model.addAttribute("deployments", new DeploymentDetailsDTO());
 
         return "index.html";
