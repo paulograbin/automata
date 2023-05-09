c9eea4d1a91857234036abfa7a4f598f44959954
Paulo Grolli Grabin <paulograbin@gmail.com>
2023-05-08 22:00:38 -0300

    Implement command line processing class

--- src/main/java/com/paulograbin/cloudportal/event/CommandLineEventListener.java
+++ src/main/java/com/paulograbin/cloudportal/event/CommandLineEventListener.java
@@ -2,34 +2,50 @@ package com.paulograbin.cloudportal.event;
 
 import com.paulograbin.cloudportal.BuildService;
 import com.paulograbin.cloudportal.DeploymentService;
+import com.paulograbin.cloudportal.ccv2.dto.CreateBuildResponseDTO;
+import com.paulograbin.cloudportal.ccv2.dto.CreateDeploymentResponseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 
 @Profile("commandline")
 @Component
-public class CommandLineEventListener
+public class CommandLineEventListener implements CommandLineRunner
 {
 	private static final Logger LOG = LoggerFactory.getLogger(CommandLineEventListener.class);
 
 	private final BuildService buildService;
 	private final DeploymentService deploymentService;
 
-	public CommandLineEventListener(BuildService buildService, DeploymentService deploymentService)
-	{
+	public CommandLineEventListener(BuildService buildService, DeploymentService deploymentService) {
 		this.buildService = buildService;
 		this.deploymentService = deploymentService;
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+        String branchName = args[0];
+        String environmentCode = args[1];
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void run() {
-		LOG.info("Running app ready event listener...");
+        if (branchName.isEmpty() || environmentCode.isEmpty()) {
+            throw new RuntimeException("Branch name and environment code cannot be empty");
+        }
 
-		buildService.monitorBuild("20230404.1");
+        LOG.info("Branch name {}", branchName);
+        LOG.info("Environment code {}", environmentCode);
+
+		CreateBuildResponseDTO createBuildResponse = buildService.createBuild(branchName);
+		buildService.monitorBuild(createBuildResponse.getCode());
+
+		LOG.info("Send deployment request");
+		CreateDeploymentResponseDTO createDeploymentResponseDTO = deploymentService.makeDeployment(createBuildResponse.getCode(), environmentCode);
+
+		LOG.info("Will start monitoring the deployment");
+		deploymentService.monitorDeployment(createDeploymentResponseDTO.getCode());
+
+        System.exit(0);
 	}
 }
