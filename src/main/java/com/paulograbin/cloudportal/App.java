74392059e425bf61eb87dbde4f65629c70db50b6
Paulo Grolli Grabin <paulograbin@gmail.com>
2023-08-24 21:37:52 -0300

    Log networking details for web app and change how profiles are checked when app starts

--- src/main/java/com/paulograbin/cloudportal/App.java
+++ src/main/java/com/paulograbin/cloudportal/App.java
@@ -1,5 +1,6 @@
 package com.paulograbin.cloudportal;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -7,8 +8,13 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.util.StringUtils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
 
 
 @SpringBootApplication
@@ -16,24 +22,68 @@ import org.springframework.util.StringUtils;
 @EnableAsync
 public class App {
 
-	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
 
-	public static void main(String[] args) {
-		if (args[0].equalsIgnoreCase("web")) {
-			LOG.warn("Starting web mode");
+    public static void main(String[] args) {
+        if (args[0].equalsIgnoreCase("web")) {
+            LOG.warn("Starting web mode");
 
-			new SpringApplicationBuilder(App.class)
-					.profiles("web")
-					.run(args);
-		}
+            SpringApplication app = new SpringApplicationBuilder(App.class)
+                    .profiles("web")
+                    .build();
 
-        LOG.warn("Starting app in commandline mode");
+            Environment environment = app.run(args).getEnvironment();
+            log(environment);
+        } else {
+            LOG.warn("Starting app in commandline mode");
 
-        new SpringApplicationBuilder(App.class)
-                .web(WebApplicationType.NONE)
-                .profiles("commandline")
-                .headless(true)
-                .run(args);
+            new SpringApplicationBuilder(App.class)
+                    .web(WebApplicationType.NONE)
+                    .profiles("commandline")
+                    .headless(true)
+                    .build();
+        }
+    }
+
+    private static void log(Environment environment) {
+        String serverPort = environment.getProperty("server.port");
+
+        String hostAddress = "localhost";
+
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            LOG.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+
+        String contextPath = Optional
+                .ofNullable(environment.getProperty("server.servlet.context-path"))
+                .filter(StringUtils::isNotBlank)
+                .orElse("/");
+
+
+        String protocol = Optional.ofNullable(environment.getProperty("server.ssl.key-store")).map(key -> "https").orElse("http");
+        String text =
+                """
+                        
+                        ----------------------------------------------------------
+                            Application '{}' is running! Access URLs:
+                            Local:     {}://localhost:{}{}
+                            External:  {}://{}:{}{}
+                            Profile(s):    {}
+                        ----------------------------------------------------------""";
+
+        LOG.info(text,
+                environment.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                environment.getActiveProfiles().length == 0 ? environment.getDefaultProfiles() : environment.getActiveProfiles()
+        );
     }
 }
