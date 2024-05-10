a8bbd85bc02087745a992e3e76c772c313e7fe0b
Paulo Grolli Grabin <paulograbin@gmail.com>
2024-05-10 11:58:06 -0300

    For release branches do not append date

--- src/main/java/com/paulograbin/cloudportal/build/BuildNameCreator.java
+++ src/main/java/com/paulograbin/cloudportal/build/BuildNameCreator.java
@@ -6,13 +6,20 @@ import java.util.Date;
 public class BuildNameCreator {
 
     public String makeBuildNameFromBranch(String branchName) {
+        String buildName = branchName;
+
+        if (buildName.contains("release/")) {
+            buildName = buildName.replace("release/", "");
+
+            return buildName;
+        }
+
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yy HH-mm");
         String formattedDate = dateFormat.format(new Date());
 
-        String buildName = branchName + " " + formattedDate;
-
-        buildName = buildName.replace("release/", "");
         buildName = buildName.replace("/", "-");
+
+        buildName = buildName + " " + formattedDate;
 
         return buildName;
     }
--- src/test/java/com/paulograbin/cloudportal/build/BuildNameCreatorTest.java
+++ src/test/java/com/paulograbin/cloudportal/build/BuildNameCreatorTest.java
@@ -1,24 +1,26 @@
 package com.paulograbin.cloudportal.build;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class BuildNameCreatorTest {
 
     private final BuildNameCreator buildNameCreator = new BuildNameCreator();
 
+
     @Test
     void releaseBranch() {
         String buildName = buildNameCreator.makeBuildNameFromBranch("release/release-8.5.1");
 
-        Assertions.assertThat(buildName).startsWith("release-8.5.1");
+        assertThat(buildName).isEqualTo("release-8.5.1");
     }
 
     @Test
     void featureBranch() {
         String buildName = buildNameCreator.makeBuildNameFromBranch("LKBENCL-4258/remove-esw-and-country-override");
 
-        Assertions.assertThat(buildName).startsWith("LKBENCL-4258-remove-esw-and-country-override");
+        assertThat(buildName).startsWith("LKBENCL-4258-remove-esw-and-country-override");
     }
 }
